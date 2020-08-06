5f41dd66c146e8d64a9783b2fb5aa3efa41ae4a8
john mutavi <jonnygovish@gmail.com>
2020-08-06 23:18:42 +0300

    add config

--- server.js
+++ server.js
@@ -2,27 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
+// Initializing the app
+const app = express();
+
 // connecting the database
-let mongodb_url = 'mongodb://localhost/';
-let dbName = 'dark';
-const MONGODB_URI = process.env.MONGODB_URI || `${mongodb_url}${dbName}`
+
+const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
 mongoose.connect(MONGODB_URI, (err)=>{
-    if (err) console.log(err)
+    if (err) {
+        console.log(err)
+    }else{
+        console.log(`Connected to Database: ${MONGODB_URI}`)
+    }
 });
 
 // test if the database has connected successfully
-let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+// let db = mongoose.connection;
+// db.once('open', ()=>{
+//     console.log('Database connected successfully')
+// })
 
-// Initializing the app
-const app = express();
+
 
 
 // View Engine
@@ -45,3 +51,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
+
+
+module.exports = app;
