f01d53e371704506e989d86df99c808016a3b21e
Brian Marete <bmarete10@gmail.com>
2020-08-13 12:58:40 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -1,8 +1,8 @@
 var config = {}
 
 config.mongoURI = {
-    production: 'mongodb://localhost/dark',
-    development: 'mongodb://localhost/dark',
-    test: 'mongodb://localhost/dark-test'
+    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
