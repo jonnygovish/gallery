57e323c0217606af6adfdf2ea68d4de29faac81b
Brian Marete <bmarete10@gmail.com>
2020-08-13 11:36:50 +0300

    Update _config.js
    Add URL to production DB

--- _config.js
+++ _config.js
@@ -2,8 +2,7 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://janyc:janojano@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://janyc:janojano@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://janyc:janojano@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    development: 'mongodb://localhost/dark',
+    test: 'mongodb://localhost/dark-test'
 }
 module.exports = config;
