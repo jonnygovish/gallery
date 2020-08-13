6f5513a0d521b3b85616e721575ae3e271fefa7e
Brian Marete <bmarete10@gmail.com>
2020-08-13 13:17:23 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -1,5 +1,6 @@
 var config = {}
 
+// Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
