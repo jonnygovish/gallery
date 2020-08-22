99a2e33f06c3e965bce377637e697d3312593e1c
tondieki <tondieki@safaricom.co.ke>
2020-08-22 21:55:39 +0300

    Add mongodb connection credentials

--- _config.js
+++ _config.js
@@ -1,9 +1,13 @@
-var config = {}
+var config = {};
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+  production:
+    "mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom?retryWrites=true&w=majority",
+  development:
+    "mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom-dev?retryWrites=true&w=majority",
+  test:
+    "mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom-test?retryWrites=true&w=majority",
+};
+module.exports = config;
 module.exports = config;
