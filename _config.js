79617648622a8c5d5415126012dc70b8c5bc49b7
Maureen Sitienei <mstienei@gmail.com>
2020-08-30 22:44:19 +0300

    update mongoURI credentials

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://galleryadmin:ThinkPad123$%@gallery.ibjbg.mongodb.net/gallery?retryWrites=true&w=majority',
-    development: 'mongodb+srv://galleryadmin:ThinkPad123$%@gallery.ibjbg.mongodb.net/gallery?retryWrites=true&w=majority',
-    test: 'mongodb+srv://galleryadmin:ThinkPad123$%@gallery.ibjbg.mongodb.net/gallery?retryWrites=true&w=majority',
+    production: 'mongodb+srv://galleryadmin:ThinkPad@gallery.ibjbg.mongodb.net/gallery?retryWrites=true&w=majority',
+    development: 'mongodb+srv://galleryadmin:ThinkPad@gallery.ibjbg.mongodb.net/gallery?retryWrites=true&w=majority',
+    test: 'mongodb+srv://galleryadmin:ThinkPad@gallery.ibjbg.mongodb.net/gallery?retryWrites=true&w=majority',
 }
 module.exports = config;
