5218c82dcf830d455043a2ca5388c9e67deecf95
Oyonka <bokejaber@gmail.com>
2022-03-23 14:01:00 +0300

    commit on 'Master'

--- _config.js
+++ _config.js
@@ -2,8 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: "mongodb+srv://<BMarasi>:<boke@gallery1>@gallery.m2cyi.mongodb.net/darkroom?retryWrites=true&w=majority",
+    development: 'mongodb+srv://<BMarasi>:<boke@gallery1>@gallery.m2cyi.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<BMarasi>:<boke@gallery1>@gallery.m2cyi.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
+
