06b26e6e6832b6ab888453bbc0a067c7a81c6409
Esther <achiengesther96@gmail.com>
2022-11-22 22:11:36 +0300

    changes made on config.js file

--- _config.js
+++ _config.js
@@ -2,8 +2,11 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://Esther:Esschichu@21@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://Esther:Esschichu@21@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Esther:Esschichu@21@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Esther:Esschichu@21@cluster0.tgiyici.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Esther:Esschichu@21@cluster0.tgiyici.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Esther:Esschichu@21@cluster0.tgiyici.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
+
+
+const uri = "mongodb+srv://Esther:<password>@cluster0.tgiyici.mongodb.net/?retryWrites=true&w=majority";
