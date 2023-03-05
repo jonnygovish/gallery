9a3675f64f996158a451a7ff5a4d70ab857e824f
wnjenga <wnjenga@safaricom.co.ke>
2023-03-05 18:38:20 +0300

    all

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
+let mongodb_url = 'mongodb+srv://wnjenga:Begood90++@cluster0.uvsqfdd.mongodb.net';
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
