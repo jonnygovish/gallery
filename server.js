7e1fb8951140f57301f6340a97ae64479df8f4be
wnjenga <wnjenga@safaricom.co.ke>
2023-03-06 20:36:57 +0300

    server.js

--- server.js
+++ server.js
@@ -2,26 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
+// Initializing the app
+const app = express();
+
 // connecting the database
-let mongodb_url = 'mongodb+srv://wnjenga:Begood90++@cluster0.uvsqfdd.mongodb.net';
+let mongodb_url = 'mongodb+srv://wnjenga:Begood90++@cluster0.uvsqfdd.mongodb.net/darkroom?retryWrites=true&w=majority/';
 let dbName = 'darkroom';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
 });
 
 // test if the database has connected successfully
-let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+// let db = mongoose.connection;
+// db.once('open', ()=>{
+//     console.log('Database connected successfully')
+// })
 
-// Initializing the app
-const app = express();
+
 
 
 // View Engine
@@ -44,3 +47,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
+
+
+module.exports = app;
