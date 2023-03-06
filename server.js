b76a7777c9fc9c36eb5623c3080dff300af851da
wnjenga <wnjenga@safaricom.co.ke>
2023-03-06 21:13:39 +0300

    server

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedT
 // test if the database has connected successfully
 let db = mongoose.connection;
 db.once('open', ()=>{
-    console.log('Database connected successfully')
+   console.log('Database connected successfully')
 })
 
 
