e473f46ef80154835a7a8c10bfb177daf399bd96
WNJENGA <111358611+WNJENGA@users.noreply.github.com>
2023-03-07 21:29:10 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://wnjenga:Begood90++@cluster0.uvsqfdd.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://wnjenga:Begood90++@cluster0.uvsqfdd.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://wnjenga:Begood90++@cluster0.uvsqfdd.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://wnjenga:darkroom2023@@cluster0.uvsqfdd.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://wnjenga:darkroom2023@@cluster0.uvsqfdd.mongodb.net/darkroom?retryWrites=true&w=majority',
+    test: 'mongodb+srv://wnjenga:darkroom2023@@cluster0.uvsqfdd.mongodb.net/darkroom?retryWrites=true&w=majority',
 }
 module.exports = config;
