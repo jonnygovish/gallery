992b6956de25c6d9a55161d0d73a5dfa887f95fb
kipkoechAllan <koecheallan@gmail.com>
2023-06-27 01:54:19 +0300

    config changes

--- _config.js
+++ _config.js
@@ -3,9 +3,9 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
 
-    production: 'mongodb+srv://user-Gallery:33492330@cluster0.whje4pz.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://user-Gallery:33492330@cluster0.whje4pz.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://user-Gallery:33492330@cluster0.whje4pz.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://gallery-User:33492330@cluster0.whje4pz.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://gallery-User:33492330@cluster0.whje4pz.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://gallery-User:33492330@cluster0.whje4pz.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 
 }
 module.exports = config;
