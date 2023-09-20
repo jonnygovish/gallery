3022667e15819eab450745fe7d93197a404eb8f7
Brayo <brianbwire5>
2023-09-20 13:01:38 +0300

    Changes to package.json and lock

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
 var config = {}
 
-// Update to have your correct username and password
+// Update with your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://brianbwire:Brayo@2788@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://brianbwire:Brayo@2788@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://brianbwire:Brayo@2788@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
+
 module.exports = config;
