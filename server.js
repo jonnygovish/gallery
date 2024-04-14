ecd02c4b20a3df9b26112f4846af900858efc647
etiti <emmanueltiti370@gmail.com>
2024-04-14 09:31:09 +0300

    adding database connection

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('./_config');
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
-
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
+let mongodb_url = config.mongoURI.production
 let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
+mongoose.connect(`${mongodb_url}${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) console.log(err)
 });
 
 // test if the database has connected successfully
 let db = mongoose.connection;
-db.once('open', ()=>{
+db.once('open', () => {
     console.log('Database connected successfully')
 })
 
@@ -39,8 +39,8 @@ app.use('/image', image);
 
 
 
- 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
