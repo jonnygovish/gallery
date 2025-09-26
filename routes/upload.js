df0a7db1f7723f0683dd9c6688247715569f416a
eve <evewangui447@gmail.com>
2025-09-26 18:19:01 +0300

    new change

--- routes/upload.js
+++ routes/upload.js
@@ -7,7 +7,7 @@ const path = require('path');
 // Set Storage engine
 const storage = multer.diskStorage({
     destination: './public/images/',
-    filename: function(req, file, cb){
+    filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     }
 })
@@ -15,8 +15,8 @@ const storage = multer.diskStorage({
 // Init upload
 const upload = multer({
     storage: storage,
-    limits: {fileSize: 1000000},
-    fileFilter: function(req,file, cb){
+    limits: { fileSize: 1000000 },
+    fileFilter: function (req, file, cb) {
         checkFileType(file, cb);
     }
 }).single('image');
@@ -24,17 +24,17 @@ const upload = multer({
 
 
 // Check file type
-function checkFileType(file, cb){
+function checkFileType(file, cb) {
     // Alowed ext
     const fileType = /jpeg|jpg|png|gif/;
     // Check ext
-    const extname = fileType.test( path.extname(file.originalname).toLowerCase());
+    const extname = fileType.test(path.extname(file.originalname).toLowerCase());
     // check mime
     const mimetype = fileType.test(file.mimetype);
 
-    if(mimetype && extname){
+    if (mimetype && extname) {
         return cb(null, true);
-    }else{
+    } else {
         cb('Error: Images Only!!')
     }
 }
