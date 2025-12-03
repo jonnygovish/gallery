bbada2526dbd4e9172dbe8bf684d22fae33c0024
richmondmwangi1 <email.com>
2025-12-03 16:13:31 +0300

    changed DB user and password

--- _config.js
+++ _config.js
@@ -1,10 +1,10 @@
 var config = {}
 
-// Update to have your correct username and password
+// Update to have your correct richmondmwangi1_db_user and mypassword@231
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://<richmondmwangi1_db_user>:<mypassword@231>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://<richmondmwangi1_db_user>:<mypassword@231>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://<richmondmwangi1_db_user>:<mypassword@231>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
 
